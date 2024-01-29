63eda4e3f1ff6a1d5c2ea802135d52b1c1bcd724
bashirking1234 <71080613+bashirking1234@users.noreply.github.com>
2024-01-29 13:45:41 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 import java.util.Scanner;
 
+/*
+ * Auteursrecht 2024 HAN University of Applied Sciences, 
+ Academie Toegepaste Biowetenschappen & Chemie. Alle rechten voorbehouden.
+ *
+ * Geschreven door Bashir Hussein <bashirking12@gmail.com>
+ *
+ * Dit bestand maakt deel uit van GenBankParserProject.
+ *
+ * GenBankParserProject is vrije software: u kunt het herverdelen en/of aanpassen
+ * onder de voorwaarden van de GNU General Public License zoals gepubliceerd door
+ * de Free Software Foundation, versie 3 van de Licentie, of
+ * (naar uw keuze) enige latere versie.
+ *
+ */
+
+
 /**
  * De Main klasse biedt een gebruikersinterface voor het interactief werken met GBFF-bestanden.
  * Gebruikers kunnen kiezen om organisme-informatie of de volledige inhoud van een GBFF-bestand te tonen.
