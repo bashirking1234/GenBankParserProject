87a706cba1104fe615eb8da5ed4fe33c19364855
bashirking1234 <71080613+bashirking1234@users.noreply.github.com>
2024-01-29 13:46:47 +0100

    Update FileParser.java

--- src/FileParser.java
+++ src/FileParser.java
@@ -1,3 +1,17 @@
+/*
+ * Auteursrecht 2024 HAN University of Applied Sciences, 
+ Academie Toegepaste Biowetenschappen & Chemie. Alle rechten voorbehouden.
+ *
+ * Geschreven door Bashir Hussein <bashirking12@gmail.com>
+ *
+ * Dit bestand maakt deel uit van GenBankParserProject.
+ *
+ * GenBankParserProject is vrije software: u kunt het herverdelen en/of aanpassen
+ * onder de voorwaarden van de GNU General Public License zoals gepubliceerd door
+ * de Free Software Foundation, versie 3 van de Licentie, of
+ * (naar uw keuze) enige latere versie.
+ *
+ */
 
 /**
  * Interface voor het parsen van bestanden, ondersteunt uitbreiding naar verschillende bestandstypen.
