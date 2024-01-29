a5efaef3526b25bad22a25a1a618769a3c9b5c21
bashirking1234 <71080613+bashirking1234@users.noreply.github.com>
2024-01-29 13:46:35 +0100

    Update OrganismInfo.java

--- src/OrganismInfo.java
+++ src/OrganismInfo.java
@@ -1,3 +1,18 @@
+/*
+ * Auteursrecht 2024 HAN University of Applied Sciences, 
+ Academie Toegepaste Biowetenschappen & Chemie. Alle rechten voorbehouden.
+ *
+ * Geschreven door Bashir Hussein <bashirking12@gmail.com>
+ *
+ * Dit bestand maakt deel uit van GenBankParserProject.
+ *
+ * GenBankParserProject is vrije software: u kunt het herverdelen en/of aanpassen
+ * onder de voorwaarden van de GNU General Public License zoals gepubliceerd door
+ * de Free Software Foundation, versie 3 van de Licentie, of
+ * (naar uw keuze) enige latere versie.
+ *
+ */
+
 
 // Klasse om informatie over organismen op te slaan.
 public class OrganismInfo {
